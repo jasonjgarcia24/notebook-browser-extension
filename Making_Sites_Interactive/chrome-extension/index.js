b074f416376c6e6580e70d15b8e311b34a26a914
JJGarcia24 <40121872+jasonjgarcia24@users.noreply.github.com>
2021-12-08 16:16:04 -0800

    Added drag-n-drop feature.

--- Making_Sites_Interactive/chrome-extension/index.js
+++ Making_Sites_Interactive/chrome-extension/index.js
@@ -1,4 +1,4 @@
-myLeads = [];
+let myLeads = [];
 const inputEl    = document.getElementById("input-el");
 const inputBtn   = document.getElementById("button-input");
 const saveTabBtn = document.getElementById("button-tab");
@@ -7,18 +7,29 @@ const ulEl       = document.getElementById("ul-el");
 const thisLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 const delete_emoji = "\u{274c}";
-const copy_emoji = "\u{1f4cb}";
-const link_emoji   = "\u{1f517}"
+const copy_emoji   = "\u{1f4cb}";
+const link_emoji   = "\u{1f517}";
+
+let base, dragging, draggedOver;
+let idMap = new Map();
 
 if (thisLocalStorage) { myLeads = thisLocalStorage; }
+if (myLeads) { setIdMap(myLeads) }
 
+
+function setIdMap(_myLeads) {
+    idMap.clear();
+
+    for (let i = 0; i < _myLeads.length; i++) {
+        idMap.set(_myLeads[i], i);
+    }
+}
 
 inputBtn.addEventListener("click", function () {
     if (!inputEl.value) { return };
 
     myLeads.push(inputEl.value);
     addToLocalStorage(myLeads);
-    renderLeads(myLeads);
 
     inputEl.value = "";
 })
@@ -26,52 +37,24 @@ inputBtn.addEventListener("click", function () {
 saveTabBtn.addEventListener("click", function () {
     chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
         myLeads.push(tabs[0].url);
-        localStorage.setItem("myLeads", JSON.stringify(myLeads));
-        renderLeads(myLeads);
+        addToLocalStorage(myLeads);
     })
-})
-
-deleteBtn.addEventListener("click", function () {
-    if (!myLeads) { return };
-
-    const msg = "Are you sure you want to delete all?";
-    if (!confirmSelection(msg)) { return };
-
-    myLeads = [];
-    clearLocalStorage();
-    renderLeads(myLeads);
 })
 
 function addToLocalStorage(_lead) {
     localStorage.setItem("myLeads", JSON.stringify(_lead));
+    renderLeads(myLeads);
+    setIdMap(myLeads);
 }
 
 function removeFromLocalStorage(e) {
-    const thisLead = e.srcElement.id;
-    let i = 0;
-    let match = false;
+    const lead = getLeadFromElementId(e.srcElement.id);
 
-    while (i < myLeads.length || !match) {
-        if (thisLead.includes(myLeads[i])) {
-            myLeads.splice(i, 1);
-            localStorage.setItem("myLeads", JSON.stringify(myLeads));
-            renderLeads(myLeads);
-            match = true;
-        }
-        i++;
-    }
-}
-
-function clearLocalStorage() {
-    localStorage.clear();
-}
-
-function confirmSelection(msg) {
-    if (confirm(msg)) {
-        return true;
-    }
-    else {
-        return false;
+    if (idMap.has(lead)) {
+        myLeads.splice(idMap.get(lead), 1);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        renderLeads(myLeads);
+        setIdMap(myLeads);
     }
 }
 
@@ -79,35 +62,75 @@ function copyLead(e) {
     const thisLead = e.srcElement.id;
     let i = 0;
     let match = false;
+    let leadCopyStr = "";
 
     while (i < myLeads.length || !match) {
         if (thisLead.includes(myLeads[i])) {
-            navigator.clipboard.writeText(myLeads[i])
+            leadCopyStr = myLeads[i].split(" ");
+            leadCopyStr = leadCopyStr[leadCopyStr.length - 1];
+
+            navigator.clipboard.writeText(leadCopyStr);
             match = true;
         }
         i++;
     }
 }
 
+function setDragging(e) {
+    dragging = getLeadFromElementId(e.target.id);
+}
+
+function setDraggedOver(e) {
+    e.preventDefault();
+    draggedOver = getLeadFromElementId(e.target.id);
+}
+
+function compare(e) {
+    myLeads.splice(idMap.get(dragging), 1);
+    myLeads.splice(idMap.get(draggedOver), 0, dragging);
+    
+    addToLocalStorage(myLeads);
+}
+
+function getLeadFromElementId(_id) {    
+    let lead = _id.split("-");
+    lead = lead[lead.length - 1];
+
+    return lead
+}
+
 function renderLeads(_myLeads) {
     let listItems = ""
-    for (let i = 0; i < _myLeads.length; i++) {        
+    let thisLead;
+    for (let i = 0; i < _myLeads.length; i++) {
+        thisLead = _myLeads[i];
         listItems += `
-            <li>
-                <span>
-                <button class="copy-lead" id="copy-${_myLeads[i]}">${copy_emoji}</button>|
-                <a class="link-lead" href="${_myLeads[i]}" target="_blank">${link_emoji}</a>|
-                <button class="delete-lead" id="delete-${_myLeads[i]}">${delete_emoji}</button>| 
-                    ${_myLeads[i]}
+            <li id="item-${thisLead}" draggable="true">
+                <span id="span-${thisLead}">
+                <button class="copy-lead" id="copy-${thisLead}">${copy_emoji}</button>|
+                <a class="link-lead" id="anchor-${thisLead}" href="${thisLead}" target="_blank">${link_emoji}</a>|
+                <button class="delete-lead" id="delete-${thisLead}">${delete_emoji}</button>| 
+                    ${thisLead}
                 </span>
             </li>
         `;
     }
     ulEl.innerHTML = listItems;
 
-    for (let i = 0; i < _myLeads.length; i++) {        
-        document.getElementById(`delete-${_myLeads[i]}`).addEventListener("click", removeFromLocalStorage);
-        document.getElementById(`copy-${_myLeads[i]}`).addEventListener("click", copyLead);
+    let listItem, deleteItem, copyItem;
+
+    for (let i = 0; i < _myLeads.length; i++) {
+        thisLead   = _myLeads[i];
+        listItem   = document.getElementById(`item-${thisLead}`);
+        deleteItem = document.getElementById(`delete-${thisLead}`);
+        copyItem   = document.getElementById(`copy-${thisLead}`);
+
+        listItem.addEventListener("drag", setDragging);
+        listItem.addEventListener("dragover", setDraggedOver);
+        listItem.addEventListener("drop", compare);
+
+        deleteItem.addEventListener("click", removeFromLocalStorage);
+        copyItem.addEventListener("click", copyLead);
     }
 }
 
